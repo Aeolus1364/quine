1fbe9ab27881c7374c98876f96928322bb1b0828
aeolus <thetesseractsshadow@gmai.com>
2018-08-12 17:45:11 -0400

    daf

--- quine.py
+++ quine.py
@@ -1,3 +1,4 @@
 code = """print('code = ?{}?, ?{}?'.format(code[0], code[1]))""", """[print(code[x]) if x == 1 else print(code[x].replace('?', '"'+'""')) for x in range(len(code))]"""
 print('code = """{}""", """{}"""'.format(code[0], code[1]))
 [print(code[x]) if x == 1 else print(code[x].replace('?', '"'+'""')) for x in range(len(code))]
+
